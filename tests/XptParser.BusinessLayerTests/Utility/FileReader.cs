9b6ca10852b5d07f68e085e25d77733b9782f84f
DESKTOP-K0MLEPA\onekr <onekryvenko.vladyslav@gmail.com>
2024-06-30 17:30:27 +0300

    Added tests for ServiceCollection and LocalMachineDocumentReader

---
+++ tests/XptParser.BusinessLayerTests/Utility/FileReader.cs
@@ -0,0 +1,20 @@
+﻿using System.IO;
+
+namespace XptParser.BusinessLayerTests
+{
+    /// <summary>
+    /// The helper class for reading documents
+    /// </summary>
+    public static class FileReader
+    {
+        /// <summary>
+        /// Reads file using provided path
+        /// </summary>
+        /// <param name="fullPath">The path to an existing file</param>
+        /// <returns>The stream representing a file</returns>
+        public static Stream ReadFile(string fullPath)
+        {
+            return File.OpenRead(fullPath); 
+        }
+    }
+}
