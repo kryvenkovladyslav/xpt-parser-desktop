c4a2c897fd457d839c5a6c9b8edbf1977cf4ee6f
DESKTOP-K0MLEPA\onekr <onekryvenko.vladyslav@gmail.com>
2024-07-02 21:52:32 +0300

    Added test for Normalizer

---
+++ tests/XptParser.BusinessLayerTests/UpperNormalizerTests.cs
@@ -0,0 +1,27 @@
+﻿using XptParser.BusinessLayer;
+using XptParser.Contracts;
+
+namespace XptParser.BusinessLayerTests
+{
+    public sealed class UpperNormalizerTests
+    {
+        private readonly INormalizer normalizer;
+
+        public UpperNormalizerTests()
+        {
+            this.normalizer = new UpperNormalizer();
+        }
+
+        [Theory]
+        [InlineData("test", "TEST")]
+        [InlineData("not", "NOT")]
+        [InlineData("aspNeT", "ASPNET")]
+        public void Normalize_WithInputStrings_ReturnsNormalizerToUpperStrings(string input, string expectedOutput)
+        {
+            var actualOutput = this.normalizer.Normalize(input);
+
+            Assert.NotNull(actualOutput);
+            Assert.Equal(expectedOutput, actualOutput);
+        }
+    }
+}
